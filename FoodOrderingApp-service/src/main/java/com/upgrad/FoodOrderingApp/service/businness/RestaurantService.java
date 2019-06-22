540ff50619cb5889d4268af45a4c2b3179395d87
deshpandesoniya16 <deshpandesoniya16@gmail.com>
2019-06-23 02:42:03 +0530

    Started with Restaurant Controller and completed Restaurant Entity files
    In Restaurant Controller
    Done with following Api
    1) Get All Restaurants - "/restaurant"
    - Retrieved all the restaurants in order of their ratings and display the response in a JSON format with the corresponding HTTP status.
    - Displayed in a categories string in alphabetical order of their category names
    2) Added Restaurant Service, Dao and Entity files also.

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantService.java
@@ -0,0 +1,34 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
+import com.upgrad.FoodOrderingApp.service.exception.InvalidRatingException;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantDao restaurantDao;
+
+
+
+    public List<RestaurantEntity> restaurantsByRating() {
+        return restaurantDao.restaurantsByRating();
+    }
+
+
+
+
+}
